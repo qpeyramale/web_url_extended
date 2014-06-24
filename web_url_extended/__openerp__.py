739787d4c3a4f5e99037da787fd5be672a6e511f
Jean-Loup <Jean-Loup@ba1fb042-9c61-4930-a97b-decf2d30055c>
2014-06-24 09:35:36 +0000

    ajout widget url for tree view

---
+++ web_url_extended/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Web/URL extended widget",
+    "description":
+        """
+        After Installation of this module, user will be directly redirect to given URL, if field's widget set to widget="url" in tree view.
+        
+        You can send feedback to info@emiprotechnologies.com,
+
+ 	modifié par sudokeys
+        """,
+    "version": "7.0",
+    "author" : "Emipro Technologies",
+    "website" : "http://www.emiprotechnologies.com",
+    "category" : "Tools",
+    "depends" : ["web"],
+    "js": [
+        "static/src/js/view_list.js",
+    ],
+    "images": ['images/web_url.png'],
+    "auto_install": False,
+}
